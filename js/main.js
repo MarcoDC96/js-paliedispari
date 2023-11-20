cd9738e641b341c384aee9d4a75a280c2df533ad
MarcoDC96 <marco.decicco@live.it>
2023-11-20 15:36:06 +0100

    Finish senza bonus

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+/*
 //Chiediamo all'utente di inserire una parola
 let inserisciParola=prompt("Inserisci una parola");
 //creiamo una variabile e la poniamo uguale alla nostra funzione al cui interno inseriamo la parola dell'utente
@@ -19,5 +20,46 @@ function palindromo(parolaUtente){
     return parolaUtenteInversa;
 
 }
+*/
+//Chiediamo all'utente di scegliere tra pari e dispari tramite prompt
+let pariDispari=prompt("Scegli tra pari e dispari");
+console.log(pariDispari);
+//Chiediamo all'utente di scegliere un numero
+let number=prompt("Scegli un numero da 1 a 5");
+//Trasformiamo in numero intero
+number = parseInt(number);
+console.log(number)
+//Riprendiamo la funzione del numero random e mettiamo che il numero massimo che deve creare è il numero 5
+let randomNumber = generateRandomNumberZeroToMaxNumber(5)
+//Creiamo la variabile somma composta dal numero random che otterremo + il numero inserito dall'utente
+let somma = number + randomNumber;
+console.log(somma);
+//Andiamo a creare una variabile associata alla funzione dove all'interno mettiamo il risultato della somma e cosa sceglie l'utente tra pari e dispari
+let risultato = sumAndOddEven(somma, pariDispari);
 
 
+//Funzione che crea un numero random con un tetto massimo scelto dall utente
+function generateRandomNumberZeroToMaxNumber(maxNumber) {
+    let randomNumber = Math.floor(Math.random() * (maxNumber + 1));
+    return randomNumber;
+}
+//Funzione che indica tramite un if se il numero è pari o dispari e le varie combinazioni per vedere se l'utente vince o perde. 
+function sumAndOddEven(numero, pariDispari1){
+if(numero%2==0 && pariDispari1=="pari"){
+console.log("Hai vinto");
+}
+
+else if(numero%2==0 && pariDispari1=="dispari"){
+console.log("Hai perso");
+}
+
+else if(numero%2!=0 && pariDispari1=="pari"){
+console.log("Hai perso");
+}
+else if(numero%2!=0 && pariDispari1=="dispari"){
+    console.log("Hai vinto");
+}
+else{
+    console.log("Ritenta");
+}
+}
